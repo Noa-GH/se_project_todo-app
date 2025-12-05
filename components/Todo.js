f29cc894e33d8b053f635a07badc7592739bb652
Noa-GH <fordnt17@gmail.com>
2025-12-05 15:22:53 -0500

    Refactor todo logic and modularize constants
    Moved initialTodos to utils/constants.js and updated scripts/index.js to import them. Added Todo class to components/Todo.js to encapsulate todo item logic. Updated index.html to use module type for scripts. Minor formatting fixes in scripts/validate.js.

---
+++ components/Todo.js
@@ -0,0 +1,53 @@
+class Todo {
+  constructor(data, templateSelector) {
+    this._data = data;
+    this._templateSelector = templateSelector;
+    this._dueDate = new Date(data.date);
+    this._element = this._getTemplate();
+    this._todoNameEl = this._element.querySelector(".todo__name");
+    this._todoCheckboxEl = this._element.querySelector(".todo__completed");
+    this._todoLabel = this._element.querySelector(".todo__label");
+    this._todoDate = this._element.querySelector(".todo__date");
+    this._todoDeleteBtn = this._element.querySelector(".todo__delete-btn");
+    // Set up the todo content and event listeners
+    this._setEventListeners();
+    this._populateTodo();
+  }
+
+  _getTemplate() {
+    const template = document.querySelector(this._templateSelector);
+    return template.content.querySelector(".todo").cloneNode(true);
+  }
+
+  _setEventListeners() {
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._element.remove();
+    });
+  }
+
+  _populateTodo() {
+    this._todoNameEl.textContent = this._data.name;
+
+    this._todoCheckboxEl.checked = this._data.completed;
+
+    // Set id and for attributes
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+
+    // Set due date if it exists
+    if (!isNaN(this._dueDate)) {
+      this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
+        "en-US",
+        {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }
+      )}`;
+    }
+  }
+
+  getView() {
+    return this._element;
+  }
+}
